fd466e1c1c0e28c69aca39b90961e092ee9bf946
Andrew Anthony <a.anthony1835@gmail.com>
2020-12-11 02:16:59 -0800

    Cleaned up comments in script.js

--- script.js
+++ script.js
@@ -77,38 +77,27 @@ function generatePassword() {
 
   }
 
-  // The below code turns myFinalArrayofCharacters into an actual array called finalArray, and then with that you're able to index
+  // The below code turns myFinalArrayofCharacters into an actual array called finalArray, and then with that I'm able to index
   var finalArray = myFinalArrayofCharacters.split("")
   console.log(finalArray)
   console.log(finalArray[40])
   console.log(finalArray.length)
-  // TODO: create a conditional to make sure that the user has chosen at least one type of character (if/else statement will be good for this). 
+  // TODO: create a conditional to make sure that the user has chosen at least one type of character (if statement will be good for this). 
   if (upperConfirm === false && lowerConfirm === false && numConfirm === false && specialCharConfirm === false ) {
     alert("You must select 'OK' on at least one of the prompts to receive a password. Try again.")
     return "CLICK BELOW TO TRY AGAIN"
   }
   
 
-  // TODO: Create a prompt to ask user how many characters they want their password to be (at least 8 characters and no more than 128) and set to a variable. You will at some point create if else statements where if they give you a value that doesn't fall between 8-128, the prompt comes back with the message "Invalid response. Response must be a number between 8 and 128" 
+  // TODO: Create a prompt to ask user how many characters they want their password to be (at least 8 characters and no more than 128) and set to a variable. If they give you a value that doesn't fall between 8-128, prompt  the message "Invalid response. Response must be a number between 8 and 128" and repeat that prompt until they give you a number between 8 and 128
   var lengthPrompt = prompt("How many characters would you like your password to be (between 8 and 128)?")
   console.log(lengthPrompt);
 
   while ((lengthPrompt>128 || lengthPrompt<8)){
     var invalidPrompt = prompt("Invalid response. Response must be a number between 8 and 128")
     lengthPrompt = invalidPrompt
-    // if ((lengthPrompt>128 || lengthPrompt<8)){
-    //   var invalidPrompt = prompt("Invalid response. Response must be a number between 8 and 128")
-    //   // lengthPrompt = invalidPrompt
-    // }
   }
 
-  // The below code doesn't work yet. It will make the prompt always go away if you put in another number, regardless of what that number's value is
-
-  // while (lengthPrompt>128 || lengthPrompt<8){
-  //   var invalidPrompt = prompt("Invalid response. Response must be a number between 8 and 128")
-  //   return invalidPrompt
-    
-  // }
 
   
 
@@ -125,13 +114,10 @@ function generatePassword() {
 
   
   // TODO: I need a for loop to loop over my final array and put the choices into the var created above. It should run the number of times that the user wants the length of the password to be (aka based on the prompt when we asked the user how many characters they like their password to be). Choose from the final array that holds all the characters that the user wanted, and save them to our finalPass variable. 
-  
-  // To do the for loop, i think you need to do some review on for loops from previous activities and see how i works and if/how it's called upon within the loop
-  // So, in the parentheses after the for loop, you're stating the paramters by which you want the stuff written in the curly brackets to run. For this, you want the for loop to run until i=lengthPrompt
-  // You're gonna write it without referring to i in the curly brackets. I don't think you need to refer to i at all because I think that i will just go up 1 each time you run the for loop, but I guess we'll see if you need to include i in the curly backets
+  // In the parentheses after the for loop, you're stating the paramters by which you want the stuff written in the curly brackets to run. For this, you want the for loop to run until i=lengthPrompt
   for (var i = 0; i < lengthPrompt; i++) {
 
-    // the below will give you a random number between 0 and finalArray length -1. I don't need to do finalArray.length-1 because Math.floor rounds it down, so even if Math.random gives me 0.9999 and I multiply that by finalArray.length, 
+    // the below will give you a random number between 0 and one less than finalArray length. I don't need to do finalArray.length-1 because Math.floor rounds it down, so even if Math.random gives me 0.9999 and I multiply that by finalArray.length, the resulting number will still be one less than finalArray.length since it's rounded down. This is perfect since I'm using this as an index and want it to be 1 less than finalArray.length so as not to return an undefined result 
     var randomNum = Math.floor(Math.random() * (finalArray.length))
     console.log(randomNum)   
 
@@ -140,13 +126,7 @@ function generatePassword() {
 
   }
   
-  // TODO: // I need to get a random number that's anywhere between 0 and finalArray.length -1. This will be the number that you index in order to pick a random character for your password. This is what will be looped
-
-  // var randomNum = Math.floor(Math.random() * (finalArray.length -1))
-  // console.log(randomNum)
-  // the above goes inside the for loop above. 
-  // (finalPass = finalPass + finalArray[randomNum])
-
+  // TODO: return the finalPass from this function. This should not be inside the for loop. Will be at the end of the function.   
   return finalPass
   
 
@@ -157,13 +137,10 @@ function generatePassword() {
 
 
 
-
-  // TODO: return the finalPass from this function. This should not be inside the for loop. Will be at the end of the function. 
-   
-  // return finalPass (or whatever variabe you decide to use that will be equal to the password)
   }
   
 
 
 // TODO: Final to do is to deal with edge cases. The only one left is if someone asks for a character amount that's less than 8 or greater than 128. 
 // Make sure it doesn't return "undefined" in the text box
+// ALL DONE!!!!!
